32bf933160f4b7d1a2fd6cd11ff72a784efeb154
Peter Haßler <pet.hassler@gmail.com>
2020-01-13 00:55:44 +0100

    Add readme and add config generation script

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 setup(name='garage_bot',
-    version='0.1',
+    version='0.2',
     description='',
-    scripts=['bin/garage_bot'],
-    packages=['garage_bot'],
+    scripts=['bin/garage_bot', 'bin/garage_bot_config'],
+    packages=['garage_bot', 'garage_bot.resources'],
+    package_data={
+        'garage_bot.resources': ['*']
+        },
     install_requires=[
         'python-telegram-bot',
         'rpi.gpio'
